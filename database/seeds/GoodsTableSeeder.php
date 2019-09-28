88907a65aa13455e2e698b5527b8dbac636212b5
pathfinder <artem.tretyakov.91@inbox.ru>
2019-09-28 22:26:57 +0300

    many

--- database/seeds/GoodsTableSeeder.php
+++ database/seeds/GoodsTableSeeder.php
@@ -38,12 +38,21 @@ class GoodsTableSeeder extends Seeder
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
             'price' => 150,
             'court_id' => FoodCourt::first()->id,
-            'category_id' => Category::BURGER,
+            'category_id' => Category::PIZZA,
         ]);
 
         Good::create([
             'title' => 'Воппер классический',
             'cooking_time' => '00:05:00',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
+            'price' => 100,
+            'court_id' => FoodCourt::first()->id,
+            'category_id' => Category::WOK,
+        ]);
+
+        Good::create([
+            'title' => 'Твистер',
+            'cooking_time' => '00:06:00',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
             'price' => 100,
             'court_id' => FoodCourt::first()->id,
@@ -56,7 +65,70 @@ class GoodsTableSeeder extends Seeder
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
             'price' => 100,
             'court_id' => FoodCourt::first()->id,
-            'category_id' => Category::WHOPPER,
+            'category_id' => Category::BURGER1,
+        ]);
+
+        Good::create([
+            'title' => 'Твистер',
+            'cooking_time' => '00:06:00',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
+            'price' => 100,
+            'court_id' => FoodCourt::first()->id,
+            'category_id' => Category::PIZZA1,
+        ]);
+
+        Good::create([
+            'title' => 'Твистер',
+            'cooking_time' => '00:06:00',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
+            'price' => 100,
+            'court_id' => FoodCourt::first()->id,
+            'category_id' => Category::WOK1,
+        ]);
+
+        Good::create([
+            'title' => 'Твистер',
+            'cooking_time' => '00:06:00',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
+            'price' => 100,
+            'court_id' => FoodCourt::first()->id,
+            'category_id' => Category::WHOPPER1,
+        ]);
+
+        Good::create([
+            'title' => 'Твистер',
+            'cooking_time' => '00:06:00',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
+            'price' => 100,
+            'court_id' => FoodCourt::first()->id,
+            'category_id' => Category::WHOPPER2,
+        ]);
+
+        Good::create([
+            'title' => 'Твистер',
+            'cooking_time' => '00:06:00',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
+            'price' => 100,
+            'court_id' => FoodCourt::first()->id,
+            'category_id' => Category::PIZZA2,
+        ]);
+
+        Good::create([
+            'title' => 'Твистер',
+            'cooking_time' => '00:06:00',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
+            'price' => 100,
+            'court_id' => FoodCourt::first()->id,
+            'category_id' => Category::BURGER2,
+        ]);
+
+        Good::create([
+            'title' => 'Твистер',
+            'cooking_time' => '00:06:00',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio leo. Donec nulla ipsum, suscipit in lacus a, aliquet hendrerit nisl. Phasellus feugiat pharetra purus ac iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et enim sit amet justo fringilla sodales. ',
+            'price' => 100,
+            'court_id' => FoodCourt::first()->id,
+            'category_id' => Category::WOK2,
         ]);
     }
 }
